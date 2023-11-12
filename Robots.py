c98c53073d5c79bac9d165460cecb58faa7e0d41
Panasewicz <145676373+panasewicz@users.noreply.github.com>
2023-11-12 20:04:10 +0100

    Branch update, cleaning

--- Robots.py
+++ Robots.py
@@ -9,23 +9,24 @@ import math
 class RobotBase:
     def __init__(self, client_id, motor_names):
         self.client_id = client_id
-        self.initialize_motors(motor_names)
+        self._initialize_motors(motor_names)
 
-    def initialize_motors(self, motor_names):
-        self.motor_handles = []
-        for name in motor_names:
-            _, handle = sim.simxGetObjectHandle(self.client_id, name, sim.simx_opmode_blocking)
-            self.motor_handles.append(handle)
+    def _initialize_motors(self, motor_names):
+        self.motor_handles = [self._get_motor_handle(name) for name in motor_names]
+
+    def _get_motor_handle(self, name):
+        _, handle = sim.simxGetObjectHandle(self.client_id, name, sim.simx_opmode_blocking)
+        return handle
 
     def move(self, speed):
-        # Set the same speed to all motors
         for handle in self.motor_handles:
-            sim.simxSetJointTargetVelocity(self.client_id, handle, speed, sim.simx_opmode_streaming)
+            self._set_motor_speed(handle, speed)
 
     def stop(self):
-        # Set speed to 0 to stop the robot
-        for handle in self.motor_handles:
-            sim.simxSetJointTargetVelocity(self.client_id, handle, 0, sim.simx_opmode_streaming)
+        self.move(0)
+
+    def _set_motor_speed(self, handle, speed):
+        sim.simxSetJointTargetVelocity(self.client_id, handle, speed, sim.simx_opmode_streaming)
 
 
 class PioneerP3DX(RobotBase):
@@ -33,46 +34,31 @@ class PioneerP3DX(RobotBase):
         super().__init__(client_id, motor_names)
 
     def turn(self, left_speed, right_speed):
-        # Ustawienie różnych prędkości dla lewego i prawego silnika
-        if len(self.motor_handles) >= 2:  # Upewniamy się, że mamy dwa silniki
-            sim.simxSetJointTargetVelocity(self.client_id, self.motor_handles[0], left_speed, sim.simx_opmode_streaming)
-            sim.simxSetJointTargetVelocity(self.client_id, self.motor_handles[1], right_speed, sim.simx_opmode_streaming)
+        if len(self.motor_handles) >= 2:
+            self._set_motor_speed(self.motor_handles[0], left_speed)
+            self._set_motor_speed(self.motor_handles[1], right_speed)
 
-    
-        
+
 class KukaYouBot(RobotBase):
-    def __init__(self, client_id, motor_names):
+    def __init__(self, client_id, motor_names, omni_wheel_names):
         super().__init__(client_id, motor_names)
-        self.initialize_omni_wheels()
+        self._initialize_omni_wheels(omni_wheel_names)
 
-    def initialize_omni_wheels(self):
-        omni_wheel_joints = ['slippingJoint_fl', 'slippingJoint_fr', 'slippingJoint_rl', 'slippingJoint_rr']
-        self.slipping_joints = []
-        for name in omni_wheel_joints:
-            _, handle = sim.simxGetObjectHandle(self.client_id, name, sim.simx_opmode_blocking)
-            self.slipping_joints.append(handle)
+    def _initialize_omni_wheels(self, omni_wheel_names):
+        self.slipping_joints = [self._get_motor_handle(name) for name in omni_wheel_names]
 
     def set_omni_wheel_orientation(self):
-        # Ustawienie orientacji stawów omni-kół
-        sim.simxSetObjectOrientation(self.client_id, self.slipping_joints[0], -1, [-math.pi/4, 0, 0], sim.simx_opmode_streaming)
-        sim.simxSetObjectOrientation(self.client_id, self.slipping_joints[1], -1, [math.pi/4, 0, math.pi], sim.simx_opmode_streaming)
-        sim.simxSetObjectOrientation(self.client_id, self.slipping_joints[2], -1, [math.pi/4, 0, math.pi], sim.simx_opmode_streaming)
-        sim.simxSetObjectOrientation(self.client_id, self.slipping_joints[3], -1, [-math.pi/4, 0, 0], sim.simx_opmode_streaming)
-        
-    def setMovement(self, speed, speed2, speed3, speed4):
-        # Zakładamy, że motor_handles[0] to lewe przednie koło, 
-        # motor_handles[1] to lewe tylne koło, itd.
-        if len(self.motor_handles) >= 4:
-            sim.simxSetJointTargetVelocity(self.client_id, self.motor_handles[0], speed, sim.simx_opmode_streaming)
-            sim.simxSetJointTargetVelocity(self.client_id, self.motor_handles[1], speed2, sim.simx_opmode_streaming)
-            sim.simxSetJointTargetVelocity(self.client_id, self.motor_handles[2], speed3, sim.simx_opmode_streaming)
-            sim.simxSetJointTargetVelocity(self.client_id, self.motor_handles[3], speed4, sim.simx_opmode_streaming)
+        orientations = [[-math.pi/4, 0, 0], [math.pi/4, 0, math.pi], [-math.pi/4, 0, 0] , [math.pi/4, 0, math.pi]]
+        for joint, orientation in zip(self.slipping_joints, orientations):
+            sim.simxSetObjectOrientation(self.client_id, joint, -1, orientation, sim.simx_opmode_streaming)
 
-    def synchronized_movement(self, speed, speed2, speed3, speed4):
-        # Najpierw ustaw orientację stawów
+    def setMovement(self, speeds):
+        for handle, speed in zip(self.motor_handles, speeds):
+            self._set_motor_speed(handle, speed)
+
+    def synchronized_movement(self, speeds):
         self.set_omni_wheel_orientation()
-        # Następnie ustaw prędkości kół
-        self.setMovement(speed, speed2, speed3, speed4)
+        self.setMovement(speeds)
 
 
 """
--- main.py
+++ main.py
@@ -10,19 +10,29 @@ try:
 
     # Tworzenie instancji robotów
     pioneer = PioneerP3DX(connection.client_id, ['Pioneer_p3dx_leftMotor', 'Pioneer_p3dx_rightMotor'])
-    youbot = KukaYouBot(connection.client_id, ['rollingJoint_fl', 'rollingJoint_rl', 'rollingJoint_rr', 'rollingJoint_fr'])
+    youbot = KukaYouBot(connection.client_id, ['rollingJoint_fl', 'rollingJoint_rl', 'rollingJoint_rr', 'rollingJoint_fr'], ['slippingJoint_fl', 'slippingJoint_rl', 'slippingJoint_rr', 'slippingJoint_fr'])
     
 
     # Skręcanie w prawo
-    youbot.synchronized_movement(-1.9, -1.9, -1.9 , -1.9)
-    time.sleep(5)
+    youbot.synchronized_movement([-4, -4, -4 , -4])
+    pioneer.move(1)
+    time.sleep(2)
 
-    youbot.setMovement(-1.9, -1.9, 2.2 , 2.2)
+    youbot.setMovement([-2.1, -2.1, 2.1 , 2.1])
     # Czekaj trochę, aby robot miał czas na skręt
-    time.sleep(2.2)
-    youbot.setMovement(-1.9, -1.9, -1.9 , -1.9)
+    time.sleep(2.1)
+    youbot.setMovement([-2.3, -2.3, -2.3 , -2.3])
     time.sleep(3.5)
     youbot.stop()
+    pioneer.turn(0,1)
+    time.sleep(1)
+    pioneer.move(1)
+    time.sleep(2.5)
+    pioneer.turn(0,1.1)
+    time.sleep(1.25)
+    pioneer.move(1)
+    time.sleep(5.4)
+    pioneer.stop()
     time.sleep(1)
 
 except Exception as e:
